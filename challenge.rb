284e532c3af81c346e04efff10abfb8e0df96aa3
sarabrent <sarabrentmccoy@gmail.com>
2015-10-20 09:23:03 -0400

    completed ruby basics assessment

--- challenge.rb
+++ challenge.rb
@@ -4,9 +4,9 @@
 # Question 1: Ruby objects and classes.
 # How would one find the class of the below objects.
 
-"Hello World"
-33
-65.89
+"Hello World".class
+33.class
+65.89.class
 
 # Question 2: Booleans
 # What would be the result of executing the below statements
@@ -17,18 +17,32 @@ else
   puts "it's false"
 end
 
+puts "it's true"
+
 # 2.1 Substitue "hello" && 4.5 for 33 && 55 above. What's the output?
+puts "it's false"
+#i'm unclear on this one...
+
 # 2.2 Substitue nil && 88 for 33 && 55 above. What's the output?
+puts "it's false"
+
 # 2.4 Substitue 63.4 < 64. What's the output?
+puts "it's true"
 
 # Question 3 :Strings
 
 # How would you (Yes, you can look up the String methods!):
 
 # 3.1. determine if a string is included another another string?
+string.include?
 # 3.2. determine if the length of a string?
+string.length
 # 3.3. Go from the string "eels in my hovercraft" to "fish in my sink" but don't
 # change the string "eels in my hovercraft"
+
+
+eels_in_my_hovercraft = 'eels in my hovercraft'
+fish_in_my_sink = eels_in_my_hovercraft.gsub('eels in my hovercraft', 'fish in my sink')
 
 
 # Question 4 : Variables
@@ -38,12 +52,36 @@ end
 vehicle = "4 wheel Bus"
 
 # 4.1 to "6 wheel Bus"
+vehicle.gsub!("4", "6")
 # 4.2 to "6 wheel tank"
+vehicle.gsub!("Bus", "tank")
 # 4.3 Create a NEW string from the string in 4.2 that will be
 # "6 wheel tank that shoots rainbow bunnnies"
+new_string = vehicle + " that shoots rainbow bunnnies"
 
 # Question 5 : Loop
 # - write a loop that guesses your age.
+age = 23
+guesses = 1
+max_guesses = 5
+answer = 0
+
+while answer != age
+  puts 'How old am I?'
+  answer = gets.chomp.to_i
+  if answer > (age + 10)
+    puts "aaaah maaan c'mon, huh, WTF. You have #{max_guesses-guesses} tries left"
+    break
+  end
+  unless guesses < max_guesses
+    puts "Too many guesses! Sorry."
+    break
+  end
+  guesses += 1
+end
+
+puts "That's it! Great guess." if answer == age
+
 # - Only allow 4 guesses
 # - Notify the user if they guess correctly OR if they run out of turns to guess
 # - Get mad and curse the fingers that thought you where that old,
